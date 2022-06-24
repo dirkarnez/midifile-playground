9d41fe8017ad1fd6b038c56f8a37e7c9f0aee293
dirkarnez <smalldirkalex@gmail.com>
2022-06-24 21:44:02 +0800

    - update src/main.cpp

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 midifile-playground
 ===================
+Based on midifile's offical code example.
+
 ### Using
 - [dirkarnez/midifile-prebuilt](https://github.com/dirkarnez/midifile-prebuilt)
 
@@ -8,3 +10,7 @@ In `build` folder after compilation using `local-build.cmd`
 ```bat
 midifile-playground.exe -o output.mid
 ```
+
+### What's include
+- [x] generate midi file
+- [x] edit midi file
--- src/main.cpp
+++ src/main.cpp
@@ -14,36 +14,59 @@ int main(int argc, char** argv) {
    options.define("x|hex=b",           "Hex byte-code output");
    options.process(argc, argv);
 
-   random_device rd;
-   mt19937 mt(rd());
-   uniform_int_distribution<int> starttime(0, 100);
-   uniform_int_distribution<int> duration(1, 8);
-   uniform_int_distribution<int> pitch(36, 84);
-   uniform_int_distribution<int> velocity(40, 100);
+   {
+      random_device rd;
+      mt19937 mt(rd());
+      uniform_int_distribution<int> starttime(0, 100);
+      uniform_int_distribution<int> duration(1, 8);
+      uniform_int_distribution<int> pitch(36, 84);
+      uniform_int_distribution<int> velocity(40, 100);
 
-   MidiFile midifile;
-   int track   = 0;
-   int channel = 0;
-   int instr   = options.getInteger("instrument");
-   midifile.addTimbre(track, 0, channel, instr);
+      MidiFile midifile;
+      int track   = 0;
+      int channel = 0;
+      int instrument   = options.getInteger("instrument");
+      midifile.addTimbre(track, 0, channel, instrument);
 
-   int tpq     = midifile.getTPQ();
-   int count   = options.getInteger("note-count");
-   for (int i=0; i<count; i++) {
-      int starttick = int(starttime(mt) / 4.0 * tpq);
-      int key       = pitch(mt);
-      int endtick   = starttick + int(duration(mt) / 4.0 * tpq);
-      midifile.addNoteOn (track, starttick, channel, key, velocity(mt));
-      midifile.addNoteOff(track, endtick,   channel, key);
+      int tpq     = midifile.getTPQ();
+      int count   = options.getInteger("note-count");
+      for (int i=0; i<count; i++) {
+         int starttick = int(starttime(mt) / 4.0 * tpq);
+         int key       = pitch(mt);
+         int endtick   = starttick + int(duration(mt) / 4.0 * tpq);
+         midifile.addNoteOn (track, starttick, channel, key, velocity(mt));
+         midifile.addNoteOff(track, endtick,   channel, key);
+      }
+
+      midifile.sortTracks();  // Need to sort tracks since added events are
+                              // appended to track in random tick order.
+      string filename = options.getString("output-file");
+      if (filename.empty()) {
+         if (options.getBoolean("hex")) midifile.writeHex(cout);
+         else cout << midifile;
+      } else
+         midifile.write(filename);
+
    }
-   midifile.sortTracks();  // Need to sort tracks since added events are
-                           // appended to track in random tick order.
-   string filename = options.getString("output-file");
-   if (filename.empty()) {
-      if (options.getBoolean("hex")) midifile.writeHex(cout);
-      else cout << midifile;
-   } else
-      midifile.write(filename);
+
+   {
+      // edit the output file, set all the notes to have velocity 127
+      string filename = options.getString("output-file");
+      if (filename.empty()) {
+         return -1;
+      }
+
+      MidiFile midifile;
+      midifile.read(filename);
+
+      for (int i=0; i<midifile[0].getSize(); i++) {
+         if (midifile[0][i].getVelocity() > 0) {
+            midifile[0][i].setVelocity(127);
+         }
+      }
+
+      midifile.write(filename + ".v2.mid");
+   }
 
    return 0;
 }
